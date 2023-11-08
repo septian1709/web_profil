278ee14a0ea724a6c94378944f0cbc4f83cc8f84
Septi <septian.17sep@gmail.com>
2023-11-08 08:36:16 +0800

    tambah href

--- app/Views/Layout/template.php
+++ app/Views/Layout/template.php
@@ -57,7 +57,7 @@
                     <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Publikasi</a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Buletin</a></li>
+                        <li><a class="dropdown-item" href="<?php echo base_url('web/layanan')">Buletin</a></li>
                         <li><a class="dropdown-item" href="https://www.youtube.com/channel/UChOunbpZLEeppdisO9m8TFA">Youtube</a></li>
                     </ul>
                     </li>
